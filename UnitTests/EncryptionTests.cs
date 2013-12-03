4f74b9f8a5c18b500c020445f70c0619d97336de
Morten Klim Sorensen <mail@kl1m.dk>
2013-12-03 09:50:00 +0100

    Solution structure

---
+++ UnitTests/EncryptionTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Security.Cryptography;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Utils.Security;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class EncryptionTests
+    {
+        [TestMethod]
+        public void TestDecrypt()
+        {
+            const string original = "Here is some data to encrypt!";
+            // Create a new instance of the AesCryptoServiceProvider 
+            // class.  This generates a new key and initialization vector (IV). 
+            var enc = new Encryption();
+            using (var myAes = new AesCryptoServiceProvider())
+            {
+                // Encrypt the string to an array of bytes. 
+                byte[] encrypted = enc.EncryptStringToBytes(original, myAes.Key, myAes.IV);
+                // Decrypt the bytes to a string. 
+                string roundtrip = enc.DecryptStringFromBytes(encrypted, myAes.Key, myAes.IV);
+
+                Assert.AreEqual(original, roundtrip);
+            }
+        }
+    }
+}
---
+++ Utils/Serialization/JsonHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Utils.Serialization
+{
+    public static class JsonHelper
+    {
+        public static T DeserializeJson<T>(string url, bool isJsonString = false)
+        {
+            string text = "";
+            if (!isJsonString)
+            {
+                var request = WebRequest.Create(url);
+                
+                var response = (HttpWebResponse) request.GetResponse();
+                if (response.GetResponseStream() != null)
+                {
+                    using (var sr = new StreamReader(response.GetResponseStream()))
+                    {
+                        text = sr.ReadToEnd();
+                    }
+                }
+            }
+            else
+            {
+                text = url.Replace("/Date(", @"\/Date(").Replace(")/", @")\/");
+            }
+
+            var jsonObj = JsonConvert.DeserializeObject<T>(text);
+            return jsonObj;
+        }
+
+        public static string SerializeJson<T>(T obj)
+        {
+            var settings = new JsonSerializerSettings() {DateFormatHandling= DateFormatHandling.MicrosoftDateFormat};
+            return JsonConvert.SerializeObject(obj, settings);
+        }
+    }
+}
