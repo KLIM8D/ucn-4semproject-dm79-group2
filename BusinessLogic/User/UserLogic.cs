b92371c6e9ca79eb71e279adf3eaf8d891f67ad3
Morten Klim Sorensen <mail@kl1m.dk>
2013-12-03 12:06:27 +0100

    EF added

---
+++ BusinessLogic/User/UserLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.User
+{
+    public class UserLogic
+    {
+        public bool SaveUser(User user)
+        {
+            
+        }
+    }
+}
--- Utils/Security/Hashing.cs
+++ Utils/Security/Hashing.cs
@@ -1,13 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Utils.Security
 {
     public class Hashing
     {
+        public string SHA512(string input)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(input);
+            byte[] hash;
+            using (SHA512 shaM = new SHA512Managed())
+            {
+                hash = shaM.ComputeHash(data);
+            }
 
+            return Encoding.UTF8.GetString(hash);
+        }
+
+        public string SHA256(string input)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(input);
+            byte[] hash;
+            using (SHA256 shaM = new SHA256Managed())
+            {
+                hash = shaM.ComputeHash(data);
+            }
+
+            return Encoding.UTF8.GetString(hash);
+        }
     }
 }
