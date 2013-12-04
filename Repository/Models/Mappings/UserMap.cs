f6ccb364c27c0a3e5a984823f0fada0f113dfa78
Morten Klim Sorensen <mail@kl1m.dk>
2013-12-04 10:43:43 +0100

    Npgsql replaced with dotConnect

--- Repository/Models/Mappings/UserMap.cs
+++ Repository/Models/Mappings/UserMap.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
@@ -7,20 +8,21 @@ using System.Threading.Tasks;
 
 namespace Repository.Models.Mappings
 {
+    [Table("UserDetails", Schema = "public")]
     public class UserMap : EntityTypeConfiguration<UserDetails>
     {
         public UserMap()
         {
             this.HasKey(t => t.Id);
 
-            this.ToTable("user_details");
+            this.ToTable("public.user_details");
             this.Property(t => t.Id).HasColumnName("usr_det-id");
-            this.Property(t => t.FirstName).HasColumnName("usr_det-Fname");
-            this.Property(t => t.SurName).HasColumnName("usr_det-Lname");
+            this.Property(t => t.FirstName).HasColumnName("usr_det-fname");
+            this.Property(t => t.SurName).HasColumnName("usr_det-lname");
             this.Property(t => t.Ssn).HasColumnName("usr_det-soc");
             this.Property(t => t.PhoneNo).HasColumnName("usr_det-phoneno");
             this.Property(t => t.EMail).HasColumnName("usr_det-email");
-            this.Property(t => t.Active).HasColumnName("usr_det-active");
+            //this.Property(t => t.Active).HasColumnName("usr_det-active");
         }
     }
 }
