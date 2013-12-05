9319be27646222277d35acfa892dbc2d4236aa7f
Morten Klim Sorensen <mail@kl1m.dk>
2013-12-05 10:28:30 +0100

    Repository added

---
+++ Repository/Resources/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository.Models;
+
+namespace Repository.Resources
+{
+    public class UserRepository
+    {
+        private RKConn db;
+
+        public UserRepository()
+        {
+            db = new RKConn();
+        }
+
+        public IQueryable<user_details> Get()
+        {
+            return db.user_details;
+        }
+    }
+}
--- UnitTests/GraphServiceTests.cs
+++ UnitTests/GraphServiceTests.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using BusinessLogic.Resources;
+using Utils.Serialization;
 
 namespace UnitTests
 {
@@ -25,6 +26,24 @@ namespace UnitTests
             var list = _graphService.GetDirections(1, 5);
 
             Assert.IsNotNull(list);
+        }
+
+        [TestMethod]
+        public void ParseJson()
+        {
+            JsonHelper.DeserializeJson<Stations>("http://geo.oiorest.dk/takstzoner.json?operat%C3%B8rnr=280");
+        }
+
+        public class Operator
+        {
+            public string nr { get; set; }
+            public string navn { get; set; }
+        }
+
+        public class Stations
+        {
+            public string nr { get; set; }
+            public Operator op { get; set; }
         }
     }
 }
