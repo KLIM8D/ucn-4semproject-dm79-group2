e71b5cb6c23529f24e1bd4a12a576f8fa7dfea85
Chris <christind@hotmail.com>
2013-12-13 11:38:37 +0100

    more commits - minor changes, much commit, wow

---
+++ Repository/Resources/WithdrawRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository.Models;
+
+namespace Repository.Resources
+{
+    public class WithdrawRepository
+    {
+        private RKConn db;
+
+        public WithdrawRepository()
+        {
+            db = new RKConn();
+        }
+
+        public int InsertWithdraw(vault_withdraws withdraw)
+        {
+            db.vault_withdraws.Add(withdraw);
+            return db.SaveChanges();
+        }
+
+        public IQueryable<vault_withdraws> GetWithdrawsByUser(int userId)
+        {
+            return db.vault_withdraws.Where(x => x.usr_det_id.Equals(userId));
+        }
+    }
+}
