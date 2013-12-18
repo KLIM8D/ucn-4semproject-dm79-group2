10d5ffdb2f8eacdd91670cc8d356395691fa6697
Chris <christind@hotmail.com>
2013-12-18 10:55:02 +0100

    wuuuuuuoooooooaaaaaaahhhhwwww

---
+++ BusinessLogic/Resources/WithdrawLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository.Models;
+using Repository.Resources;
+
+namespace BusinessLogic.Resources
+{
+    public class WithdrawLogic
+    {
+        public bool SaveDeposit(vault_withdraws withdraw)
+        {
+            var withdrawRepository = new WithdrawRepository();
+            withdrawRepository.InsertWithdraw(withdraw);
+
+            return true;
+        }
+
+        public IQueryable<vault_withdraws> GetWithdrawsByUserId(int userId)
+        {
+            var withdrawRepository = new WithdrawRepository();
+            return withdrawRepository.GetWithdrawsByUser(userId);
+        }
+    }
+}
